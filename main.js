f0fbc5587ef8ad2691a82da945b0dddc9a698fec
daors12345678910 <daors.sagonjeva@gmail.com>
2025-12-16 20:37:46 +0100

    hah

--- main.js
+++ main.js
@@ -1,41 +1,8 @@
 
-function validation() {
-   
-    var name = document.getElementById("name").value.trim();
-    
-    
-    var valid_name_regex = /^[A-Za-z]+$/;
+$('#sort').sortable();
 
-    if (name.match(valid_name_regex)) {
-        
-        document.getElementById("name_error").style.visibility = "hidden";
-        document.getElementById("name").style.borderColor = "#198754"; 
-        return true;
-    } else {
-        
-        document.getElementById("name_error").style.visibility = "visible";
-        document.getElementById("name").style.borderColor = "#8719199f";
-        return false;
-    }
-}
-
-
-document.getElementById("name").addEventListener("input", function() {
-    this.value = this.value.replace(/[^A-Za-z]/g, "");
+$(function(){
+	$("#accordion").accordion();
 });
 
-
-$(document).ready(function() {
-    $('.tab-btn').click(function() {
-   
-        $('.tab-btn').removeClass('active');
-        $('.content').removeClass('active');
-
-     
-        $(this).addClass('active');
-
- 
-        const target = $(this).data('target');
-        $('#' + target).addClass('active');
-    });
-});
+$('#tabs').tabs();
