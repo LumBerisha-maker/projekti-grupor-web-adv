746a1c49c16be1d07855930915be62c5c0b1724a
daors12345678910 <daors.sagonjeva@gmail.com>
2025-12-18 18:40:14 +0100

    final

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 
+  
+  input.style.padding = "8px";
+  input.style.border = "2px solid gray";
+  input.style.borderRadius = "5px";
+
+  msg.style.display = "none";
+  msg.style.color = "red";
+  msg.style.fontWeight = "bold";
+
+
 $('#sort').sortable();
 
 $(function(){
@@ -6,3 +16,21 @@ $(function(){
 });
 
 $('#tabs').tabs();
+
+document.getElementById("submitBtn").addEventListener("click", function () {
+  const name = document.getElementById("name").value.trim();
+  const regex = /^[A-Za-z\s'-]+$/;
+  const errorElement = document.getElementById("error");
+
+  if(name.match(valid_name_regex)){
+
+
+         return true;
+          
+      }else{
+
+
+         document.getElementById("error").style.visibility = "visible";
+         document.getElementById("error").style.borderColor = "red";
+         return false;
+      }
